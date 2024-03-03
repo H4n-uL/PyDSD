a3d2e7da0e1c194f7ce47ad9839f3f51dd3159dd
h4n-ul <jun061119@gmail.com>
2024-03-03 14:40:17 +0900

    ~DEPR~ :: Depricated demodulator

--- src/tools/dsig.py
+++ src/tools/dsig.py
@@ -19,16 +19,3 @@ class DeltaSigma:
             bitstream[i] = 1 if self.quant==1 else 0
 
         return np.packbits([int(b) for b in bitstream.astype(np.uint8)])
-
-    def demodulator(self, bitstream):
-        bitstream = np.unpackbits(bitstream)
-        y = np.zeros_like(bitstream, dtype=np.float64)
-
-        for i in range(len(bitstream)):
-            self.quant = 1 if bitstream[i] == 1 else -1
-            for j in range(self.deg-1, 0, -1):
-                self.intg[j] = self.intg[j-1]
-            self.intg[0] = self.quant
-            y[i] = self.intg[-1]
-
-        return y
